155aa15fd42f61097c9808e5f4b8e95e736b45fd
Luis <luis.espinoza.nav@live.com>
2020-03-02 14:02:07 -0300

    Captured Form

--- src/app/component/create-contact/create-contact.component.ts
+++ src/app/component/create-contact/create-contact.component.ts
@@ -1,31 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create-contact',
-  templateUrl: './create-contact.component.html',
-  styleUrls: ['./create-contact.component.css']
+  selector: "app-create-contact",
+  templateUrl: "./create-contact.component.html",
+  styleUrls: ["./create-contact.component.css"]
 })
 export class CreateContactComponent implements OnInit {
-
   isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
+  firstFormGroup_FULLNAME: FormGroup;
+  secondFormGroup_CONTACT_INFO: FormGroup;
+  thirdFormGroup_ADDRESS: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(private _formBuilder: FormBuilder) {}
 
-  ngOnInit() {
+  submit() {
+    console.log(
+      `FUll NAME ${JSON.stringify(this.firstFormGroup_FULLNAME.value)}
+        `
+    );
 
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-    this.thirdFormGroup = this._formBuilder.group({
-      thirdCtrl: ['', Validators.required]
-    });
+    console.log(
+      `CONTACT INFO${JSON.stringify(this.secondFormGroup_CONTACT_INFO.value)}`
+    );
+
+    console.log(
+      `ADDRESS ${JSON.stringify(this.thirdFormGroup_ADDRESS.value)}`
+    );
+    
   }
 
+  ngOnInit() {
+    this.firstFormGroup_FULLNAME = this._formBuilder.group({
+      title: ["", Validators.required],
+      name: ["", Validators.required],
+      lastname: ["", Validators.required]
+    });
+    this.secondFormGroup_CONTACT_INFO = this._formBuilder.group({
+      phone: ["", Validators.required],
+      cell: ["", Validators.required],
+      email: ["", Validators.required]
+    });
+    this.thirdFormGroup_ADDRESS = this._formBuilder.group({
+      country: ["", Validators.required],
+      city: ["", Validators.required],
+      streetName: ["", Validators.required],
+      streetNumber: ["", Validators.required]
+    });
+  }
 }
